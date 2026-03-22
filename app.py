553960492c712c1f9b614920ad5ec226919954c2
Pratik Prajapati <prajapatipratik20@gmail.com>
2026-03-22 14:26:59 -0400

    Fixing linting

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -11,3 +12,6 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
